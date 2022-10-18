6a76a63301828167115eb97bde6f79bc02319e12
qiyuanchen <chenqiyuan1012@foxmail.com>
2022-10-18 17:21:21 +0800

    can not solve in 3.5 d and e

--- ch3/assignment.R
+++ ch3/assignment.R
@@ -1,3 +1,5 @@
+library(MASS)
+
 ave_model = function(x, y) {
   mu = sum(y) / length(y)
   y_hat = mu
@@ -14,22 +16,23 @@ linear_model = function(x, y) {
 
 quadratic_model = function(x, y) {
   ones = rep(1, length(x))
-  x_quad = x * x
+  x_quad = x ^ 2
   x = cbind(ones, x, x_quad)
+  #print(dim(x))
   mu = solve(t(x) %*% x) %*% t(x) %*% y # 求参数
   y_hat = x %*% mu # 预测
   return(list(mu, y_hat))
 }
 
 season_model_with_intercept = function(x, y) {
-  x = model.matrix(~ x, x)
+  x = model.matrix( ~ x, x)
   mu = solve(t(x) %*% x) %*% t(x) %*% y # 求参数
   y_hat = x %*% mu # 预测
   return(list(mu, y_hat))
 }
 
 season_model_without_intercept = function(x, y) {
-  x = model.matrix(~ x - 1, x) # 转化为one-hot编码
+  x = model.matrix( ~ x - 1, x) # 转化为one-hot编码
   mu = solve(t(x) %*% x) %*% t(x) %*% y # 求参数
   y_hat = x %*% mu # 预测
   return(list(mu, y_hat))
@@ -64,3 +67,51 @@ season_model_with_intercept(x, y) # 验证有截距情况
 season_model_without_intercept(x, y) # 验证没有截距情况
 # x=harmonic(tempdub,1) # 书本做法
 cosine_model(x, y)
+
+
+
+# 3.5
+# a
+data("wages")
+win.graph(width = 8,
+          height = 8,
+          pointsize = 10)
+plot(
+  x = zlag(wages),
+  y = wages,
+  xlab = "Previous Year Wages",
+  ylab = "Wages"
+)
+win.graph(width = 12,
+          height = 6,
+          pointsize = 10)
+plot(wages, type = 'o', pch = 21)
+# b
+x = time(wages)
+y = wages
+
+result = linear_model(x, y)
+y_hat = result[[2]]
+y_num = as.numeric(y) # 转换为数值
+e = y_num - y_hat
+# c
+win.graph(12, 6, pointsize = 10)
+plot(ts(e, start = start(wages), frequency = 12),
+     type = 'o',
+     pch = 21)
+# d
+lm2 = lm(wages ~ time(wages) + I(time(wages) ^ 2), singular.ok = FALSE)
+x = time(wages)
+y = wages
+result = quadratic_model(x, y)
+y_hat = result[[2]]
+y_num = as.numeric(y) # 转换为数值
+e = y_num - y_hat
+# e
+win.graph(12, 6, pointsize = 10)
+plot(ts(e, start = start(wages), frequency = 12),
+     type = 'o',
+     pch = 21)
+
+
+
