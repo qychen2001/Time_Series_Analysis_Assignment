9ac4418b09bdb9e5c2d0e48f1af0ed7863e700cf
qiyuanchen <chenqiyuan1012@foxmail.com>
2022-11-28 21:44:56 +0800

    11-28

--- ch6/acf_func.R
+++ ch6/acf_func.R
@@ -1,4 +1,5 @@
 acf_func = function(series) {
+  # 样本自相关函数计算函数
   n = length(series)
   series_mean = mean(series)
   series = series - series_mean
@@ -10,43 +11,58 @@ acf_func = function(series) {
   return(acf_result)
 }
 
-pacf_func = function(series) {
-  n = length(series)
-  t = n
-  for (k in 1:(n-1)) {
-    
+pacf_func_1 = function(series, max_lag = 20) {
+  # 偏自相关的另外一种实现 6.2.9
+  rho = acf_func(series) # 得到样本自相关函数
+  phi = matrix(0, nrow = max_lag, ncol = max_lag) #phi是一个函数
+  phi[1, 1] = rho[1]
+  for (k in 2:max_lag) {
+    a = rho[k] - phi[(k - 1), 1:(k - 1)] %*% rho[(k - 1):1]
+    b = 1 - phi[(k - 1), 1:(k - 1)] %*% rho[1:(k - 1)]
+    phi[k, k] = a / b
+    for (j in 1:(k - 1)) {
+      phi[k, j] = phi[k - 1, j] - phi[k, k] * phi[k - 1, k - j]
+    }
   }
+  return(diag(phi))
+}
+
+pacf_func = function(series, max_lag = 20) {
+  # 6.2.8的实现
+  rho = acf_func(series) # 得到样本自相关函数
+  M = matrix(1, nrow = max_lag, ncol = max_lag) #phi是一个函数
+  for (i in 1:(max_lag - 1)) {
+    for (j in (i + 1):max_lag) {
+      M[i, j] = rho[j - i]
+      M[j, i] = rho[j - i] #对称矩阵
+    }
+  }
+  # 得到完整的矩阵
+  phi_kk = rep(rho[1], max_lag)
+  for (k in 2:max_lag) {
+    result = qr.solve(M[1:k, 1:k], rho[1:k])
+    phi_kk[k] = result[k]
+  }
+  return(phi_kk)
 }
 
 
 
 MA_thm_acf = function(MA_params) {
-  q=length(MA_params)
-  rho0=1+(MA_params%*%MA_params)
-  rho = rep(0,(q+1))
-  if (q > 1){
-    for (k in 1:(q-1)){
-      rho[k]=(-MA_params[k]+(MA_params[1:(q-k)]%*%MA_params[(k+1):q]))/rho0
+  q = length(MA_params)
+  rho0 = 1 + (MA_params %*% MA_params)
+  rho = rep(0, (q + 1))
+  if (q > 1) {
+    for (k in 1:(q - 1)) {
+      rho[k] = (-MA_params[k] + (MA_params[1:(q - k)] %*%
+                                   MA_params[(k + 1):q])) / rho0
     }
   }
-  rho[q]= (-MA_params[q])/rho0
+  rho[q] = (-MA_params[q]) / rho0
   return(rho)
 }
 
-
-
-
-# 
-# library(TSA)
-# data(ima22.s)
-# acf_func(ima22.s)
-# acf(ima22.s)
-# acf(ima22.s)$acf
-# 
-# source("./sm_series.R")
-# data("ma1.1.s")
-# length(ma1.1.s)
-# acf(ma1.1.s)$acf
-# acf_func(ma1.1.s)
-# sm_ma1=ARMA_func(NULL,c(0.9),120)
-# acf(sm_ma1)$acf
+draw_acf = function(acf_series, type = "acf") {
+  plot(acf_series, type = 'h')
+  abline(h = 0)
+}
